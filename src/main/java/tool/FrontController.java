2fee764fbae962cdedc75d519b80c03c4f0f1bba
c_shinooka <c_shinooka@LAPTOP-NF7VQ234>
2024-04-25 11:56:13 +0900

    エラー内容が見えるように修正(完成したらコメント通りに修正する)

--- src/main/java/tool/FrontController.java
+++ src/main/java/tool/FrontController.java
@@ -1,6 +1,7 @@
 package tool;
 
 import java.io.IOException;
+import java.io.PrintWriter;
 
 import jakarta.servlet.ServletException;
 import jakarta.servlet.annotation.WebServlet;
@@ -14,14 +15,15 @@ public class FrontController extends HttpServlet {
 	protected void doGet(
 		HttpServletRequest request, HttpServletResponse response
 	) throws ServletException,IOException {
+		PrintWriter out=response.getWriter();	// 完成したら消す
 		try {
 			String path=request.getServletPath().substring(1);
 			String name=path.replace(".a", "A").replace('/', '.');
 			Action action=(Action)Class.forName(name).getDeclaredConstructor().newInstance();
 			action.execute(request,  response);
 		} catch (Exception e) {
-			e.printStackTrace();
-			request.getRequestDispatcher("/error.jsp").forward(request, response);
+			e.printStackTrace(out);		// 完成したらoutを消す
+			// request.getRequestDispatcher("/error.jsp").forward(request, response);
 		}
 	}
 	
