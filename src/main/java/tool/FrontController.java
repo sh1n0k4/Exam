29a477b9c40aaf2bcf492a8268cff611e8dd7c2d
c_shinooka <c_shinooka@LAPTOP-NF7VQ234>
2024-04-25 11:50:57 +0900

    エラーページへの遷移を追記

--- src/main/java/tool/FrontController.java
+++ src/main/java/tool/FrontController.java
@@ -1,7 +1,6 @@
 package tool;
 
 import java.io.IOException;
-import java.io.PrintWriter;
 
 import jakarta.servlet.ServletException;
 import jakarta.servlet.annotation.WebServlet;
@@ -11,23 +10,24 @@ import jakarta.servlet.http.HttpServletResponse;
 
 @WebServlet(urlPatterns= {"*.action"})
 public class FrontController extends HttpServlet {
-	public void doPost(
+	@Override
+	protected void doGet(
 		HttpServletRequest request, HttpServletResponse response
 	) throws ServletException,IOException {
-		PrintWriter out=response.getWriter();
 		try {
 			String path=request.getServletPath().substring(1);
 			String name=path.replace(".a", "A").replace('/', '.');
 			Action action=(Action)Class.forName(name).getDeclaredConstructor().newInstance();
 			action.execute(request,  response);
 		} catch (Exception e) {
-			e.printStackTrace(out);
+			e.printStackTrace();
+			request.getRequestDispatcher("/error.jsp").forward(request, response);
 		}
 	}
 	
-	public void doGet(
+	public void doPost(
 		HttpServletRequest request, HttpServletResponse response
 	) throws ServletException, IOException {
-		doPost(request, response);
+		doGet(request, response);
 	}
 }
